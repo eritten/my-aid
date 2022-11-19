e3ff7186c9d61d4ea12856bbf36649e01665613b
eritten <eritten2@gmail.com>
2022-11-19 19:41:07 +0000

    adding disability and volunteer types to the database

--- community/models.py
+++ community/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+VOLUNTEER_TYPE = (('GENRAL', 'GENERAL'), ('HEALTH', 'HEALTH'), ('BANKING', 'BANKING'), ('LEGAL RIGHT', 'LEGAL RIGHT'), ('MESSENGER', 'MESSENGER'), ('COUNCELLER', 'COUNCELLER'), ('TRANSPORT', 'TRANSPORT'))
+DISABILITY_TYPE = (("VISUALLY IMPAIRMENT", "VISUALLY IMPAIRMENT"), ("PHYSICALLY CHALLENGE", "PHYSICALLY CHALLENGE"),
+("DEAF AND DAM", "DEAF AND DAM"),
+("DOWN SYNDROME", "DOWN SYNDROME")
+) 
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -15,6 +21,11 @@ class Profile(models.Model):
     occupation = models.CharField(max_length=200, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile images', null=True, blank=True)
+    create_profile = models.BooleanField(default=False)
+    volunteer_type = models.CharField(max_length=100, choices=VOLUNTEER_TYPE)
+    disability_type = models.CharField(max_length=100, choices=DISABILITY_TYPE)
+
     def __str__(self):
         return self.user.username
+
 

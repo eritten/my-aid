442f6ef25c2f147dd253bc7e372a951a86740601
= <=>
2022-11-26 21:27:26 +0000

    worked

---
+++ homepage/static/js/main.js
@@ -0,0 +1,10 @@
+// aos initialisation
+AOS.init({
+    offset: 100,
+    delay: 100,
+    duration: 700,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom'
+})
--- myaid/urls.py
+++ myaid/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from homepage import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 path('comment/', include('comment.urls')),
@@ -9,3 +11,7 @@ path('blog/', include('blog.urls')),
 path('app/', include('community.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

46a937daf7fd5ea9038f246e47ecdf796447449b
= <=>
2022-11-27 11:27:58 +0000

    finisished

--- homepage/static/js/main.js
+++ homepage/static/js/main.js
@@ -8,3 +8,31 @@ AOS.init({
     mirror: false,
     anchorPlacement: 'top-bottom'
 })
+window.addEventListener("load", ()=>{
+
+    //the about video functionality
+  const video = document.querySelector("#dis-vid");
+  const playBtn = document.querySelector("#vid-btn");
+
+
+//   playBtn.addEventListener("click", () => {
+//     const hasPlay = playBtn.classList.contains("play");
+//     console.log(video)
+//     if (hasPlay) {
+//       playVid();
+//     } else {
+//       pauseVid();
+//     }
+//   });
+  
+//   function playVid() {
+//     video.play();
+//     playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
+//     playBtn.classList.remove("play");
+//   }
+//   function pauseVid() {
+//     video.pause();
+//     playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+//     playBtn.classList.add("play");
+//   }
+})
